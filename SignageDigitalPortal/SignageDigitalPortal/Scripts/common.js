3cefdd1de6ec15b5d265775e870a2ccf52d027b5
Israel <israelvi@hotmail.com>
2014-03-31 20:53:18 -0600

    Reconstrucción del sitio

---
+++ SignageDigitalPortal/SignageDigitalPortal/Scripts/common.js
@@ -0,0 +1,17 @@
+﻿window.relMouseCoords = function (e) {
+    var el = e.target, c = el;
+    var scaleX = c.width / c.offsetWidth || 1;
+    var scaleY = c.height / c.offsetHeight || 1;
+
+    if (!isNaN(e.offsetX))
+        return { x: e.offsetX * scaleX, y: e.offsetY * scaleY };
+
+    var x = e.pageX, y = e.pageY;
+    do {
+        x -= el.offsetLeft;
+        y -= el.offsetTop;
+        el = el.offsetParent;
+    } while (el);
+    return { x: x * scaleX, y: y * scaleY };
+};
+
